6d7d0db6b43a8688c3b311307a8dd85cb04a8d9e
FLeuthold <42573303+FLeuthold@users.noreply.github.com>
2018-10-25 16:59:11 +0200

    Deleted unused code parts

--- texture.cpp
+++ texture.cpp
@@ -1,26 +1,3 @@
-/*
-* Copyright © 2012-2015 Graham Sellers
-*
-* Permission is hereby granted, free of charge, to any person obtaining a
-* copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-* and/or sell copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice (including the next
-* paragraph) shall be included in all copies or substantial portions of the
-* Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
-*/
-
 #include <sb7.h>
 #include <vmath.h>
 #include <sb7ktx.h>
@@ -133,22 +110,6 @@ public:
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 	}
 
-private:
-	void generate_texture(float * data, int width, int height)
-	{
-		int x, y;
-
-		for (y = 0; y < height; y++)
-		{
-			for (x = 0; x < width; x++)
-			{
-				data[(y * width + x) * 4 + 0] = (float)((x & y) & 0xFF) / 255.0f;
-				data[(y * width + x) * 4 + 1] = (float)((x | y) & 0xFF) / 255.0f;
-				data[(y * width + x) * 4 + 2] = (float)((x ^ y) & 0xFF) / 255.0f;
-				data[(y * width + x) * 4 + 3] = 1.0f;
-			}
-		}
-	}
 
 private:
 	GLuint      texture;
